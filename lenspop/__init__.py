83f34177f8433731bfe12380d27f8803a7618949
jiwoncpark <jiwon.christine.park@gmail.com>
2018-08-30 00:45:19 -0700

    Try keeping lenspop init empty

--- lenspop/__init__.py
+++ lenspop/__init__.py
@@ -1,7 +1,7 @@
-from __future__ import absolute_import, print_function, division
-__all__ = ['population_functions', 'make_lens_pop', 'surveys', 'fast_lens_sim', 'distances']
-from lenspop.population_functions import *
-from lenspop.make_lens_pop import *
-from lenspop.surveys import *
-from lenspop.fast_lens_sim import *
-from lenspop.distances import *
+#from __future__ import absolute_import, print_function, division
+#__all__ = ['population_functions', 'make_lens_pop', 'surveys', 'fast_lens_sim', 'distances']
+#from lenspop.population_functions import *
+#from lenspop.make_lens_pop import *
+#from lenspop.surveys import *
+#from lenspop.fast_lens_sim import *
+#from lenspop.distances import *
